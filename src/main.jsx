82d0b3a09eb21097e02bb8543b953d564fd97ffb
Alieza Nuriman <muhammadalieza4@gmail.com>
2023-05-09 00:11:29 +0700

    feat: setting up Apollo client and Browser router in the project's root (main)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { ApolloProvider } from '@apollo/client'
 import App from './App'
+import client from './apollo/apolloClient'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>,
 )
