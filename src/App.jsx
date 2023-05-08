60d4f1e18e399dd464c0f613e7258a74eef6ea72
Alieza Nuriman <muhammadalieza4@gmail.com>
2023-05-09 04:29:45 +0700

    fix: routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Progress from './pages/Progress';
+import MoreGoals from './pages/MoreGoals';
+import MoreHabits from './pages/MoreHabits';
 
 function App() {
   return (
-    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/progress' element={<Progress />} />
+      <Route path='/yourgoals' element={<MoreGoals />} />
+      <Route path='/yourhabits' element={<MoreHabits />} />
+    </Routes>
   );
 }
 
