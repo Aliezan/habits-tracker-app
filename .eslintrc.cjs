1873082a96275dba26de803d620ca44c67a8d88e
Alieza Nuriman <muhammadalieza4@gmail.com>
2023-05-09 00:06:48 +0700

    fix: exclude some rules in eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,11 +17,15 @@ module.exports = {
   plugins: [
     'react',
   ],
+
   ignorePatterns: ["tailwind.config.js"],
+
   rules: {
     "import/no-extraneous-dependencies": "off",
     "import/no-named-as-default": "off",
-    "import/no-named-as-default-member": "off"
+    "import/no-named-as-default-member": "off",
+    "react/forbid-prop-types": "off",
+    "react/jsx-boolean-value": "off",
 
   },
 };
